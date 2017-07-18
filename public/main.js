8dbffeb9cceaa606484d86bd968fd1a8ab3533aa
hutingting119 <1912257350@qq.com>
2017-07-18 10:33:05 +0800

    add showAll

--- public/main.js
+++ public/main.js
@@ -8,18 +8,41 @@ function addNewUser() {
     $.ajax({
         type: 'post',
         url: '/insert',
-        data:JSON.stringify({
-            name:name,
-            age:age,
-            sex:sex,
-            phone:phone,
-            email:email,
-            remark:remark
+        data: JSON.stringify({
+            name: name,
+            age: age,
+            sex: sex,
+            phone: phone,
+            email: email,
+            remark: remark
         }),
 
         contentType: "application/json; charset=utf-8",
         success: function (result) {
             alert(result)
+        }
+    })
+}
+
+function showAll() {
+    $.ajax({
+        type: 'post',
+        url: '/showall',
+        contentType: 'application/json;charset=utf-8',
+        success: function (result) {
+            for (var i = 0; i < result.length; i++) {
+                let name = result[i].name;
+                let age = result[i].age;
+                let sex = result[i].sex;
+                let phone = result[i].phone;
+                let email = result[i].email;
+                let remark = result[i].remark;
+                var addTr = document.createElement('tr');
+                addTr.innerHTML = "<td>" + name + "</td>" + "<td>" + age + "</td>" + "<td>" + sex + "</td>" + "<td>" + phone
+                    + "</td>" + "<td>" + email + "</td>" + "<td>" + remark + "</td>"
+                    + "<td><span  onclick='del(this)'><img src='img/del.png'></span></td>";
+                document.getElementById('tables').appendChild(addTr);
+            }
         }
     })
 
--- server.js
+++ server.js
@@ -7,10 +7,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 const insert = require("./mysql/insert");
+const showall=require('./mysql/showall');
 
 app.use(express.static('public'));
 
 app.use("/", insert);
+app.use('/',showall);
 
 var server = app.listen(3000, () => {
     console.log('listening at port %s', server.address().port);
